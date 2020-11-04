395cefd86e0b0fea06266f1a87ccfe93b980e2a7
Mateus Duarte <mateusduarte@Mateuss-MacBook-Pro.local>
2020-11-04 19:04:42 -0200

    implementação do componente header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { AppContainer } from './components/AppContainer'
-
+import {Header} from './components/Header'
 function App() {
 	return (
-        <AppContainer />
+        <Header/>
 	)
 }
 
