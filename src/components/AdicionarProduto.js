67c351c297ea3c438fa875ff4bbe07242fe6a7bc
Michael Soto <michael_nickolas@hotmail.com>
2020-11-06 14:07:03 -0300

    Ajustei o componente AdicionarProduto

--- src/components/AdicionarProduto.js
+++ src/components/AdicionarProduto.js
@@ -1,51 +1,53 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  background-color: #f0efeb;
-  padding: 20px;
+const Div = styled.div`
 `;
 
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items:center;
-  margin-bottom: 8px;
-  padding: 8px;
+const Container = styled.div`
+display:flex;
+flex-direction:column;
+  background-color: #f0efeb;
+  padding: 20px;
+  margin:5rem 10rem 10rem 12rem;
+
 `;
+
+
 
 const Div2 = styled.div``;
 
 const Titulo = styled.h3`
+margin-top:3rem;
+font-size:2rem;
+display:flex;
+justify-content:center;
   color: #00bfb2;
 `;
 
 const Label = styled.label`
-  margin-left: 10px;
-  
+ margin:1rem 0.4rem;
 `;
 
 const Input = styled.input`
-  display: column;
-  justify-content: center;
-  margin-left: 10px;
-  width: 10rem;
-`;
+`
 
 const Select = styled.select`
-  margin-left: 10px;
+ margin:2rem 0.5rem;
 `;
 
 const Button = styled.button`
   color: white;
+  
   border-radius: 4px;
   border: none;
   height: 7vh;
-  width: 20vw;
+  width: 8rem;
   font-weight: 500;
   font-size: 1.1em;
   background-color: #00bfb2;
+  margin-top:1rem;
+  
   :focus {
     outline: none;
   }
@@ -54,60 +56,51 @@ const Button = styled.button`
     background-color: #72efdd;
   }
   :active {
-    background-color: red;
-    box-shadow: 0 1px #666;
-    transform: translateY(4px);
+    background-color: #139188;
+        box-shadow: 0 1px #666;
+        transform: translateY(4px);
   }
 `;
 
 class CadProduto extends React.Component {
   render() {
     return (
-      <div>
-        <Titulo>Vamos cadastrar seus produtos!</Titulo>
+      <Div>
+        <Titulo>Vamos cadastrar seu produtos!</Titulo>
+
         <Container>
-          <InputContainer>
-            <Label>Nome</Label>
-            <Input />
-          </InputContainer>
+          <Label>Nome</Label>
+          <Input />
+          <Label>Descrição</Label>
+          <Input />
 
-          <InputContainer>
-            <Label>Descrição</Label>
-            <Input />
-          </InputContainer>
+          <Div2>
+          <Label>Preço</Label>
+          <Input />
+          <Label>Categoria</Label>
+          <Select>
+            <option value="roupas">Roupas</option>
+            <option value="artigos">Artigos</option>
+            <option value="calcados">Calçados</option>
+            <option value="eletronicos">Eletrônicos</option>
+            <option value="moveis">Móveis</option>
+          </Select>
 
-          <InputContainer>
-            <Div2>
-              <Label>Preço</Label>
-              <Input />
+          <Label>Meio de Pagamento</Label>
+          <Select>
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Crédito</option>
+            <option value="debito">Debito</option>
+          </Select>
+          </Div2>
 
-              <Label>Categoria</Label>
-              <Select>
-                <option value="roupas">Roupas</option>
-                <option value="artigos">Artigos</option>
-                <option value="calcados">Calçados</option>
-                <option value="eletronicos">Eletrônicos</option>
-                <option value="moveis">Móveis</option>
-              </Select>
+          <Label>Url Imagem</Label>
+          <Input />
 
-              <Label>Meio de Pagamento</Label>
-              <Select>
-                <option value="dinheiro">Dinheiro</option>
-                <option value="credito">Crédito</option>
-                <option value="debito">Debito</option>
-              </Select>
-            </Div2>
-          </InputContainer>
-
-          <InputContainer>
-            <Label>Url Imagem</Label>
-            <Input />
-          </InputContainer>
           <Button>Anunciar</Button>
         </Container>
-      </div>
+      </Div>
     );
   }
 }
-
 export default CadProduto;
